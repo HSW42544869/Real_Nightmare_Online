344f1c9099e68e4dbb30132acbaf56816ab324da
Yushain <73380147+YutianGG@users.noreply.github.com>
2021-05-09 13:32:32 +0800

    攝影機追蹤
    5/9

---
+++ Real_Nightmare_Online/Assets/Script/CamaraCtrl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraCtrl : MonoBehaviour
+{
+    [Header("追蹤物件")]
+    public Transform target;
+    [Header("移動速度")]
+    public float speed = 2;
+    private void Update()
+    {
+        Track();
+    }
+
+    /// <summary>
+    /// 物件追蹤
+    /// </summary>
+    private void Track()
+    {
+        Vector2 posA = target.position;     // 目標座標
+        Vector2 posB = transform.position;  // 攝影機座標
+
+        posB = Vector2.Lerp(posB, posA,Time.deltaTime * speed); 
+        transform.position = posB;                                      
+    }
+
+}
--- Real_Nightmare_Online/Assets/Script/player.cs
+++ Real_Nightmare_Online/Assets/Script/player.cs
@@ -2,13 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    
-    private void Awake()
-    {
-       
-    }
     // Start is called before the first frame update
     void Start()
     {
