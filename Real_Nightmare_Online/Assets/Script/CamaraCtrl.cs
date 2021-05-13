8f42d3c9963f1c98c3e9f508b9b08b2890725542
Yushain <73380147+YutianGG@users.noreply.github.com>
2021-05-14 01:49:12 +0800

    5/14
    #角色移動
    #角色旋轉
    #攝影機移動並限制位置

--- Real_Nightmare_Online/Assets/Script/CamaraCtrl.cs
+++ Real_Nightmare_Online/Assets/Script/CamaraCtrl.cs
@@ -8,6 +8,10 @@ public class CamaraCtrl : MonoBehaviour
     public Transform target;
     [Header("移動速度")]
     public float speed = 2;
+    [Header("限制")]
+    public Vector2 height = new Vector2(0, 20.65f);
+    public Vector2 width = new Vector2(0, 27);
+
     private void Update()
     {
         Track();
@@ -17,12 +21,14 @@ public class CamaraCtrl : MonoBehaviour
     /// 物件追蹤
     /// </summary>
     private void Track()
-    {
-        Vector2 posA = target.position;     // 目標座標
-        Vector2 posB = transform.position;  // 攝影機座標
+    {   
+        Vector3 posA = target.position;     // 目標座標
+        Vector3 posB = transform.position;  // 攝影機座標
+
+        posA.y = Mathf.Clamp(posA.y, height.x, height.y); // 判斷上下距離
+        posA.x = Mathf.Clamp(posA.x, width.x, width.y);   // 判斷左右距離
 
         posB = Vector2.Lerp(posB, posA,Time.deltaTime * speed); 
-        transform.position = posB;                                      
+        transform.position = posB;  // 設定新攝影機座標      
     }
-
 }
