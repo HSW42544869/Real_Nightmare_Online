f95b38203d3740e9ccc3b2fc5ab410eb7cbade7e
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-05-14 11:44:14 +0800

    5/14 11.

---
+++ Real_Nightmare_Online/Assets/Script/FieldOfview.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+
+public class FieldOfview : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        float fov = 90f;
+        Vector3 origin = Vector3.zero;
+        int rayCount = 2;
+        float angle = 0f;
+        float angleIncrease = fov / rayCount;
+        float viewDistance = 50f;
+
+
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        for (int i = 0; i < rayCount; i++)
+        {
+            Vector3 vertex = origin + UtilsClass.GetVectorFromAngle(angle) * viewDistance;
+        }
+
+        vertices[0] = Vector3.zero;
+        vertices[1] = new Vector3(50, 0);
+        vertices[2] = new Vector3(0, -50);
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+
+
+    }
+
+}
