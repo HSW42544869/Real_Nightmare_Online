d0023d2220b70a81b255e6b94abddaf65735d619
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-05-16 23:49:14 +0800

    5/16 23.

--- Real_Nightmare_Online/Assets/Script/FieldOfview.cs
+++ Real_Nightmare_Online/Assets/Script/FieldOfview.cs
@@ -6,22 +6,26 @@ using CodeMonkey.Utils;
 
 public class FieldOfview : MonoBehaviour
 {
+    [SerializeField] private LayerMask layerMask;   //用來偵測敵人,如果是敵人射線無遮擋遮擋
     private Mesh mesh;
+    private float fov;
+    private float viewDistance;
+    private Vector3 origin;
+    private float startingAngle;
     // Start is called before the first frame update
     void Start()
     {
-        Mesh mesh = new Mesh();
+        mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-    }
-    private void Update()
-    {
-
-        float fov = 90f; //攻擊預設角度
+        fov = 40f; //攻擊預設角度
         Vector3 origin = Vector3.zero;
+    }
+    private void LateUpdate()
+    {
         int rayCount = 50;   //弧線設50個
-        float angle = 0f;   //角度
+        float angle = startingAngle ;   //角度
         float angleIncrease = fov / rayCount;   //間隔距離公式
-        float viewDistance = 50f;   //距離
+        float viewDistance = 80f;   //距離
 
 
         Vector3[] vertices = new Vector3[rayCount + 1 + 1]; //向量頂點
@@ -35,7 +39,7 @@ public class FieldOfview : MonoBehaviour
         for (int i = 0; i < rayCount; i++)  //設定預判區域所捕捉到的物件
         {
             Vector3 vertex;
-            RaycastHit2D raycastHit2D = Physics2D.Raycast(origin, UtilsClass.GetVectorFromAngle(angle), viewDistance);  //射線區域,回Utils程式庫抓取計算出徑度,查看距離
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(origin, UtilsClass.GetVectorFromAngle(angle), viewDistance, layerMask);  //射線區域,回Utils程式庫抓取計算出徑度,查看距離
             if (raycastHit2D.collider == null)  //如果區域內沒有任何物件    
             {
                 //No hit
@@ -51,7 +55,7 @@ public class FieldOfview : MonoBehaviour
             }
             vertices[vertexIndex] = vertex;
 
-            if (i > 0)
+            if (i > 0)  //
             {
                 triangles[triangleIndex + 0] = 0;
                 triangles[triangleIndex + 1] = triangleIndex - 1;
@@ -75,8 +79,25 @@ public class FieldOfview : MonoBehaviour
         mesh.vertices = vertices;
         mesh.uv = uv;
         mesh.triangles = triangles;
+        mesh.bounds = new Bounds(origin, Vector3.one * 1000f);
 
+    }
 
+    public void SetOrigin(Vector2 origin)
+    {
+        this.origin = origin;
+    }
+    public void SetAimDirection(Vector3 aimDirection)
+    {
+        startingAngle = UtilsClass.GetAngleFromVectorFloat(aimDirection) - fov / 2f;
+    }
+    public void SetFov(float fov)
+    {
+        this.fov = fov;
+    }
+    public void SetviewDistance(float viewDistance)
+    {
+        this.viewDistance = viewDistance;
     }
 
 }
