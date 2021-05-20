d6b802395dddb5d72d29402a9403b785a96066c1
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-05-21 00:32:59 +0800

    5/21

--- Real_Nightmare_Online/Assets/Script/player.cs
+++ Real_Nightmare_Online/Assets/Script/player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Player : MonoBehaviour
+public class player : MonoBehaviour
 {
     /// <summary>
     /// 角色狀態
